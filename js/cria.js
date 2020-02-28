977435de2c6da060b976c82c5daf6f173d38c33c
Mauro <maurojh@gmail.com>
2020-02-28 20:12:27 -0300

    not exists

--- js/cria.js
+++ js/cria.js
@@ -4,8 +4,7 @@ var db = openDatabase('banco', '1.0', 'primeiro banco', 2 * 1024 * 1024);
 
 // transaction: se algo der errado não altera o banco:
 db.transaction(function (tx) {
-    tx.executeSql('CREATE TABLE consumos (id INTEGER PRIMARY KEY AUTOINCREMENT, litros TEXT NOT NULL, km TEXT NOT NULL)');
-    tx.executeSql('INSERT INTO consumos (id, litros, km) VALUES (1, "5.5", "40.0")');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS consumos (id INTEGER PRIMARY KEY AUTOINCREMENT, litros TEXT NOT NULL, km TEXT NOT NULL)');
     var saida = document.getElementById("saida");
     saida.innerHTML = "Tabela criada";
 });
